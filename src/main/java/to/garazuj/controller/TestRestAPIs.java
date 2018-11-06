0b76169e74fe88306d9bcd9c109552735623325d
Przemysław Gędźba <przemyslaw.gedzba@gmail.com>
2018-11-06 18:43:26 +0100

    Obtaining current user data

--- src/main/java/to/garazuj/controller/TestRestAPIs.java
+++ src/main/java/to/garazuj/controller/TestRestAPIs.java
@@ -1,9 +1,16 @@
 package to.garazuj.controller;
 
+import org.apache.catalina.security.SecurityUtil;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import to.garazuj.model.User;
+import to.garazuj.security.services.UserPrinciple;
+
+import java.security.Principal;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -20,4 +27,12 @@ public class TestRestAPIs {
 	public String adminAccess() {
 		return ">>> Admin Contents";
 	}
+
+	@GetMapping("/api/test/currentuser")
+    public UserPrinciple getCurrentUser(Principal principal) {
+
+        return ((UserPrinciple) SecurityContextHolder.getContext()
+                .getAuthentication()
+                .getPrincipal());
+    }
 }
