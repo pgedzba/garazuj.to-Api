cc026dc423236e48f237931411699f456c9f6db9
Damian-Czarnota <yziek@windowslive.com>
2018-11-07 15:49:29 +0100

    Adds method to edit logged user

--- src/main/java/to/garazuj/controller/TestRestAPIs.java
+++ src/main/java/to/garazuj/controller/TestRestAPIs.java
@@ -1,20 +1,34 @@
 package to.garazuj.controller;
 
 import org.apache.catalina.security.SecurityUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import to.garazuj.message.request.EditUserForm;
 import to.garazuj.model.User;
+import to.garazuj.repository.UserRepository;
 import to.garazuj.security.services.UserPrinciple;
 
 import java.security.Principal;
+import java.util.Optional;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 public class TestRestAPIs {
+	
+	@Autowired
+	UserRepository userRepository;
 	
 	@GetMapping("/api/test/user")
 	@PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
@@ -35,4 +49,17 @@ public class TestRestAPIs {
                 .getAuthentication()
                 .getPrincipal());
     }
+	
+	@PutMapping("/api/test/currentuser")
+	public @ResponseBody ResponseEntity editUser(@RequestParam Long id, @RequestBody EditUserForm form) {
+		User u = null;
+		u = userRepository.findById(id).get();
+		if(!form.getFirstName().isEmpty())
+			u.setFirstName(form.getFirstName());
+		if(!form.getLastName().isEmpty())
+			u.setLastName(form.getLastName());
+		userRepository.save(u);
+		
+		return new ResponseEntity(HttpStatus.OK);
+	}
 }
