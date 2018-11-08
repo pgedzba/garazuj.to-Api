588b1a60894b4441a94b72370b8b87bc97bd88bd
Damian-Czarnota <yziek@windowslive.com>
2018-11-08 10:32:22 +0100

    Adds EditUserForm

---
+++ src/main/java/to/garazuj/message/request/EditUserForm.java
@@ -0,0 +1,14 @@
+package to.garazuj.message.request;
+
+ import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+ import lombok.Data;
+ @Data
+public class EditUserForm {
+    @Size(min=3, max = 60)
+    private String firstName;
+     @Size(min = 6, max = 40)
+    private String lastName;
+  
+}
