678c083dd104bcabd518c94172898c049f28689f
Damian-Czarnota <yziek@windowslive.com>
2018-11-25 19:30:05 +0100

    New methods to get cars and delete car

--- src/main/java/to/garazuj/controller/CarRestAPIs.java
+++ src/main/java/to/garazuj/controller/CarRestAPIs.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import to.garazuj.message.request.AddCarForm;
 import to.garazuj.message.request.SignUpForm;
@@ -13,9 +14,14 @@ import to.garazuj.model.Role;
 import to.garazuj.model.RoleName;
 import to.garazuj.model.User;
 import to.garazuj.repository.CarRepository;
+import to.garazuj.security.services.UserPrinciple;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
+import java.beans.Transient;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -26,10 +32,14 @@ public class CarRestAPIs {
 	@Autowired
 	CarRepository carRepository;
 
-	@PostMapping("/add")
+	@PostMapping()
 	public ResponseEntity<?> addCar(@Valid @RequestBody AddCarForm addCarForm) {
 		Car car = new Car();
+		User user = ((UserPrinciple) SecurityContextHolder.getContext()
+				.getAuthentication()
+				.getPrincipal()).getUser();
 
+		car.setOwner(user);
 		car.setBrand(addCarForm.getBrand());
 		car.setModel(addCarForm.getModel());
 		car.setType(addCarForm.getType());
@@ -42,6 +52,28 @@ public class CarRestAPIs {
 		carRepository.save(car);
 
 		return new ResponseEntity<>(new ResponseMessage("Car added successfully!"), HttpStatus.OK);
+	}
+
+	@GetMapping()
+	public List<Car> getCars(){
+		List<Car> cars;
+		User user = ((UserPrinciple) SecurityContextHolder.getContext()
+				.getAuthentication()
+				.getPrincipal()).getUser();
+		if(carRepository.findByOwnerId(user.getId()).isPresent())
+			cars =carRepository.findByOwnerId(user.getId()).get();
+		else
+			cars = Collections.emptyList();
+
+		return cars;
+	}
+
+	@Transactional
+	@DeleteMapping()
+	public ResponseEntity<?> deleteCar(@RequestParam Long index){
+		carRepository.deleteCarById(index);
+
+		return new ResponseEntity<>(new ResponseMessage("Car deleted successfully!"), HttpStatus.OK);
 	}
 
 }
