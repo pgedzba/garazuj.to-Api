fdc707f3a5bc690900d287da0dee9d57ff131046
Damian-Czarnota <yziek@windowslive.com>
2018-12-13 11:57:56 +0100

    Adds users handler

---
+++ src/main/java/to/garazuj/services/UsersService.java
@@ -0,0 +1,28 @@
+package to.garazuj.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import to.garazuj.exception.CarException;
+import to.garazuj.exception.UserException;
+import to.garazuj.model.User;
+import to.garazuj.repository.UserRepository;
+
+@Service
+public class UsersService {
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	public List<User> getAllUsers(){
+		return userRepository.findAll();
+	}
+	
+	public User getUser(Long id) {
+		return userRepository.findById(id)
+                .orElseThrow(() -> new UserException("User not found " + id));
+	}
+
+}
