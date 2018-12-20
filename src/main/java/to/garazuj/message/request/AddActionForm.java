447045b21a0556276d86a30e87131b8fe7d996ae
Damian-Czarnota <yziek@windowslive.com>
2018-12-20 11:30:06 +0100

    Adds form to adding history's action

---
+++ src/main/java/to/garazuj/message/request/AddActionForm.java
@@ -0,0 +1,25 @@
+package to.garazuj.message.request;
+
+import java.util.Date;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import lombok.Data;
+
+@Data
+public class AddActionForm {
+	
+	@NotNull
+	private Float price;
+	
+	private Long carID;
+	
+	private Long userID;
+	
+	@NotBlank
+	private String description;
+	
+	@NotBlank
+	private Date date;
+}
